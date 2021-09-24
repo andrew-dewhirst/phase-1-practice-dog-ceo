d8a9de2574ea324cdbddd951e299646b8debad10
Andrew Dewhirst <andrew.dewhirst8@gmail.com>
2021-09-23 22:42:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', event => {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(response => response.json()) 
+  .then(dogData => dogData.forEach(dog => showDogs(dog)))
+});
+
+function showDogs(){
+  console.log('hello')
+}
